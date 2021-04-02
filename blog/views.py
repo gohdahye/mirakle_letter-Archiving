14fef4ecab6527fb81aed7c4eafc3aff819a028f
gohdahye <kdhnate222@nate.com>
2021-04-02 13:27:05 +0900

    FBV 블로그 포스 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,24 @@ from .models import Post
 # Create your views here.
 
 def index(request):
-
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
         'blog/index.html',
         {
             'posts': posts,
+        }
+    )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
         }
     )
