6dc3ceabb8641646a6d6024634c545809710bf3e
gohdahye <kdhnate222@nate.com>
2021-04-06 17:13:06 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 
@@ -8,30 +9,23 @@ from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama', password='somepassword')
 
     def navbar_test(self, soup):
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
-        logo_btn = navbar.find('a', text='Do It Django')
-        self.assertEqual(logo_btn.attrs['href'], '/')
 
-        home_btn = navbar.find('a', text='Home')
-        self.assertEqual(home_btn.attrs['href'], '/')
-
-        blog_btn = navbar.find('a', text='Blog')
-        self.assertEqual(blog_btn.attrs['href'], '/blog/')
-
-        about_me_btn = navbar.find('a', text='About Me')
-        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/blog/')
         # 1.2 정상적으로 페이지가 로드된다.
         self.assertEqual(response.status_code, 200)
-        # 1.3 페이지 타이틀은 'Blog'이다.
+
+        # 1.3 페이지 타이틀은 Blog 이다
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
 
@@ -49,11 +43,13 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author= self.user_trump
         )
 
         post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
+            author=self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -69,12 +65,15 @@ class TestView(TestCase):
 
         # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
 
     def test_post_detail(self):
         # 1.1. 포스트가 하나 있다.
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다. ',
             content='Hello World. We are the world.',
+            author=self.user_trump,
         )
         # 1.2. 그 포스트의 url은 '/blog/1/' 이다.
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
@@ -97,7 +96,7 @@ class TestView(TestCase):
         self.assertIn(post_001.title, post_area.text)
 
         # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다.(아직 구현할 수 없음)
-        #아직 작성 불가
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
 
         # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text)
