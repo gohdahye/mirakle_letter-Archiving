d7cf7f011351c1ccee6ed4cb73cb66d9e7eae0b1
gohdahye <kdhnate222@nate.com>
2021-04-12 15:49:42 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from markdownx.models import MarkdownxField
 from markdownx.utils import markdown
 import os
 
+
 # Create your models here.
 
 
@@ -64,3 +65,16 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
-
+from .models import Post, Category, Tag, Comment
 
 # Create your tests here.
 
@@ -43,6 +42,12 @@ class TestView(TestCase):
         )
         self.post_003.tags.add(self.tag_python_kor)
         self.post_003.tags.add(self.tag_python)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_obama,
+            content='첫 번째 댓글입니다.'
+        )
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -145,10 +150,16 @@ class TestView(TestCase):
         # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(self.post_001.content, post_area.text)
 
-        # 2.7. hello 태그가 존재하는지, python, 파이썬공부가 존재하지 않는지 확
+        # 2.7. hello 태그가 존재하는지, python, 파이썬공부가 존재하지 않는지 확인
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
+        # comment area
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
 
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
