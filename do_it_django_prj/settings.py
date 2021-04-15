ce48016b4c989ffe3e54ad091a877997d12de02f
gohdahye <kdhnate222@nate.com>
2021-04-15 15:35:44 +0900

    자기소개 페이지에 내용 채우기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = False
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
