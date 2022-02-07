0082399c3f108f8972f69ddf043c94559a349f1b
gohdahye <kdhnate222@nate.com>
2022-02-07 16:34:11 +0900

    회원가입 disabled

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', views.login, name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('signup/', views.signup, name="signup"),
+   # path('signup/', views.signup, name="signup"),
     path('agreement/', views.agreement, name="agreement"),
    # path('profile/', views.profile, name='profile'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
