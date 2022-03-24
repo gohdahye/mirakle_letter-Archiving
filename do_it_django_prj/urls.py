fc624c05395977311c2f89beb074986c2ae52f10
gohdahye <kdhnate222@nate.com>
2022-03-24 15:38:42 +0900

    favicon

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 handler404 = 'blog.views.error_404_view'
 handler500 = 'blog.views.error_500_view'
@@ -29,6 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls')),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
